000fd250506a1892571196ef988aca5ef2e1ad44
cheewu <cheewu@126.com>
2018-09-17 10:08:44 +0800

    update config

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,12 +1,12 @@
 /**
  * Created by cheewu on 2018/9/9.
  */
-const dbPath = 'mongodb://localhost:27017/test'
-
-module.exports = {
+const conf = {
   dbPath: 'mongodb://localhost:27017/test',
   connectConf: {
     poolSize: 40,
     keepAlive: 120
   }
 }
+
+module.exports = conf
