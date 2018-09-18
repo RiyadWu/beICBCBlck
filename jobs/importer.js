6f770cbc288f291c5cb2d3ded43bbb3bba8ce847
cheewu <cheewu@126.com>
2018-09-19 00:34:36 +0800

    refactor

--- jobs/importer.js
+++ jobs/importer.js
@@ -10,9 +10,29 @@ const { Mcc } = require('../models')
 const logger = require('../utils/logger')
 const conf = require('../config/job')
 
-const batchSize = 50000
+const batchSize = 5000
 const fileProcessed = {}
+let unprocessedFile = []
+let fileProcessing = false
+let taskQueue = []
+let resolving = false
 
+function tryResolveTask() {
+  logger.log('try resolve task')
+  if (!resolving) {
+    resolving = true
+    resolveTask()
+  }
+}
+
+function resolveTask() {
+  logger.log('resolve task')
+  if (taskQueue.length > 0) {
+    bulkInsert(taskQueue.splice(0, batchSize), resolveTask)
+  } else {
+    resolving = false
+  }
+}
 
 function getFileList(path, callback) {
   fs.readdir(path, (err, files) => {
@@ -24,79 +44,86 @@ function getFileList(path, callback) {
   })
 }
 
-function bulkInsert(bulkToInsert) {
+function bulkInsert(bulkToInsert, cb) {
   Mcc.insertMany(bulkToInsert, function (err) {
     if (err) {
       logger.log('Bulk insert error! Error is: ', err)
     } else {
       logger.log('Bulk insert done!')
+      cb()
     }
   })
 }
 
-function bulkInsertPromise(arr, start = 0) {
-  const end = start + batchSize
-  Mcc.insertMany(arr.slice(start, end))
-    .then(() => {
-      logger.log('Bulk insert done!')
-      if (start < arr.length) {
-        bulkInsertPromise(arr, end)
+function tryProcessFile() {
+  logger.log('try process file!')
+  if (!fileProcessing) {
+    fileProcessing = true
+    processFile()
+  } else {
+    fileProcessing = false
+  }
+}
+
+function processFile() {
+  logger.log('process file!')
+  if (unprocessedFile.length === 0) {
+    setTimeout(() => {
+      if (unprocessedFile.length === 0) {
+        tryResolveTask()
       }
-    }, () => {
-      logger.log('Bulk insert error! Error is: ', err)
+    }, 5000)
+    return
+  }
+
+  const file = unprocessedFile.splice(0, 1)[0]
+
+  if (fileProcessed[file]) {
+    console.log('file has processed: ', file)
+    processFile()
+  } else {
+    fileProcessed[file] = true
+    readFile(file, processFile)
+  }
+}
+
+function readFile(file, cb) {
+  logger.log('read file: ', file)
+  const month = file.substr(0, 8)
+  const processedDir = conf.processedDataFileDir
+  const processedPath = processedDir + file
+  const path = conf.txtDataFileDir + file
+  const lineReader = readline.createInterface({
+    input: fs.createReadStream(path).pipe(iconv.decodeStream('gbk'))
+  })
+
+  lineReader.on('line', function (line) {
+    const [mcc, shop] = line.split(',')
+    taskQueue.push({ mcc, shop, month })
+  })
+
+  lineReader.on('close', function () {
+    logger.log('file close: ', file)
+
+    if (!fs.existsSync(processedDir)) {
+      fs.mkdirSync(processedDir)
+    }
+
+    mv(path, processedPath, function(err) {
+      if (err) {
+        logger.log('mv file failed.')
+      } else {
+        logger.log(`File ${file} has been moved successfully.`)
+      }
+      cb()
     })
+  })
 }
 
 function importData() {
   getFileList(conf.txtDataFileDir, files => {
-    files.forEach(file => {
-      if (fileProcessed[file]) {
-        console.log('file has processed: ', file)
-        return
-      } else {
-        fileProcessed[file] = true
-      }
-
-      const month = file.substr(0, 8)
-      const processedDir = conf.processedDataFileDir
-      const processedPath = processedDir + file
-      const path = conf.txtDataFileDir + file
-      const lineReader = readline.createInterface({
-        input: fs.createReadStream(path).pipe(iconv.decodeStream('gbk'))
-      })
-      let arr = []
-
-      lineReader.on('line', function (line) {
-        const [mcc, shop] = line.split(',')
-        arr.push({ mcc, shop, month })
-        if (arr.length === batchSize) {
-          const bulkToInsert = [].concat(arr)
-
-          bulkInsert(bulkToInsert)
-          arr = []
-        }
-      })
-
-      lineReader.on('close', function () {
-        logger.log('file close')
-        if (arr.length > 0) {
-          bulkInsert(arr)
-          // bulkInsertPromise(arr)
-        }
-
-        if (!fs.existsSync(processedDir)) {
-          fs.mkdirSync(processedDir)
-        }
-
-        mv(path, processedPath, function(err) {
-          if (err) {
-            logger.log('mv file failed.')
-          } else {
-            logger.log(`File ${file} has been moved successfully.`)
-          }
-        })
-      })
-    })
+    unprocessedFile = unprocessedFile.concat(files)
+    tryProcessFile()
   })
 }
 
@@ -106,7 +133,7 @@ function start() {
   const watcher = chokidar.watch(dataDir)
 
   watcher.on('change', path => {
-    logger.log(path)
+    logger.log('file change: ', path)
     importData()
   })
 }
